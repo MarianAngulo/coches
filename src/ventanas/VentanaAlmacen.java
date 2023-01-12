8576b188b5ba81e89e8a1a0ebc614eed9b1fdc27
MarianAngulo <81021160+MarianAngulo@users.noreply.github.com>
2023-01-12 20:59:39 +0100

    Más cosas del admin

--- src/ventanas/VentanaAlmacen.java
+++ src/ventanas/VentanaAlmacen.java
@@ -1,7 +1,11 @@
 package ventanas;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
@@ -15,8 +19,23 @@ public class VentanaAlmacen extends JFrame {
 		
 		JPanel panelSup = new JPanel();
 		panelSup.setLayout(new BorderLayout());
-		panelSup.add(barra, BorderLayout.NORTH);
+		panelSup.add(barra);
 		getContentPane().add( panelSup, BorderLayout.NORTH );
 		
+		JButton btnVolver = new JButton("Volver");
+		barra.add(btnVolver);
+		
+		
+		btnVolver.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				VentanaAdmin va = new VentanaAdmin();
+				va.setSize(600, 600);
+				va.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				va.setVisible(true);
+				dispose();
+			}
+		});
 	}
 }
--- src/ventanas/VentanaUsuarios.java
+++ src/ventanas/VentanaUsuarios.java
@@ -1,13 +1,24 @@
 package ventanas;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JToolBar;
+
+import clases.Usuario;
 
 public class VentanaUsuarios extends JFrame {
 	private JToolBar barra;
-	
+	private ArrayList<Usuario> usuarios = new ArrayList<>();
 	
 	public VentanaUsuarios() {
 		barra = new JToolBar();
@@ -17,5 +28,69 @@ public class VentanaUsuarios extends JFrame {
 		panelSup.add(barra, BorderLayout.NORTH);
 		getContentPane().add( panelSup, BorderLayout.NORTH );
 		
+		JPanel panelCentral = new JPanel();
+		getContentPane().add( panelCentral, BorderLayout.CENTER );
+		panelCentral.setLayout(null);
+		
+		JButton btnVolver = new JButton("Volver");
+		barra.add(btnVolver);
+		
+		JButton btnAgregar = new JButton("Agregar");
+		btnAgregar.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		btnAgregar.setBounds(50, 450, 200, 37);
+		panelCentral.add(btnAgregar);
+		
+		JButton btnEliminar = new JButton("Eliminar");
+		btnEliminar.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		btnEliminar.setBounds(300, 450, 200, 37);
+		panelCentral.add(btnEliminar);
+
+		JList listaUsuarios = new JList();
+		listaUsuarios.setBounds(50, 54, 487, 234);
+		panelCentral.add(listaUsuarios);
+		
+		/*
+		JScrollPane scroll = new JScrollPane(listaUsuarios);
+		panelCentral.add(scroll);
+		*/
+		
+		btnVolver.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				VentanaAdmin va = new VentanaAdmin();
+				va.setSize(600, 600);
+				va.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				va.setVisible(true);
+				dispose();
+			}
+		});
+		
+		btnAgregar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String name = JOptionPane.showInputDialog("Intruduzca un nombre de usuario");
+				String con = JOptionPane.showInputDialog("Intruduzca una contraseña");
+	
+				if (name.isEmpty() || con.isEmpty()) {
+					JOptionPane.showMessageDialog(null, "No puedes dejar campos vacios.");
+				}
+				//Confirmar que no existe un usuario con ese nombre
+				
+				//Guardarlo en la base de datos
+			}
+		});
+		
+		btnEliminar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				usuarios.remove(listaUsuarios.getSelectedIndex());
+				//eliminar de la BD
+				
+				repaint();
+			}
+		});
 	}
 }
