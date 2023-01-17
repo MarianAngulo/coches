4bc6b8c219c9e9837fa25e9ee31f71394d2a70e1
XabiGomez <xabier.gomezu@opendeusto.es>
2023-01-17 19:35:13 +0100

    Imagenes falla

--- src/ventanas/VentanaVehiculos.java
+++ src/ventanas/VentanaVehiculos.java
@@ -1,6 +1,8 @@
 package ventanas;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,19 +15,23 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Vector;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JToolBar;
 import javax.swing.ListSelectionModel;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 import clases.MarcaVehiculo;
 import clases.TipoVehiculo;
 import clases.Vehiculo;
+
 
 public class VentanaVehiculos extends JFrame {
 	
@@ -107,18 +113,18 @@ public class VentanaVehiculos extends JFrame {
 	
 	private void initTabla() {
 
-		Vector<String> cabeceras = new Vector<String>(Arrays.asList( "ID", "TIPO", "MARCA", "MODELO","PRECIO"));
+		Vector<String> cabeceras = new Vector<String>(Arrays.asList( "ID", "TIPO", "MARCA", "MODELO","PRECIO", "IMAGEN"));
 		modelo = new DefaultTableModel(new Vector<Vector<Object>>(), cabeceras);
 		tabla = new JTable(modelo);
 		tabla.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		
+		tabla.getColumnModel().getColumn(5).setCellRenderer(renderImagen);
 	}
 	
 	private void cargarVehiculos() {
 		modelo.setRowCount(0);
 		
 		for (Vehiculo v : listvehiculos) {
-			this.modelo.addRow( new Object[] {v.getId(), v.getTipo(), v.getMarca(), v.getModelo(), v.getPrecio()} );
+			this.modelo.addRow( new Object[] {v.getId(), v.getTipo(), v.getMarca(), v.getModelo(), v.getPrecio(), v.getUrl()} );
 		}		
 	}
 	
@@ -159,6 +165,23 @@ public class VentanaVehiculos extends JFrame {
 		ventVe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ventVe.setResizable(false);
 	}
+	
+	DefaultTableCellRenderer renderImagen = new DefaultTableCellRenderer() {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+			JLabel label = new JLabel();
+			
+			 for(Vehiculo v: listvehiculos) {
+				 label.setIcon(new ImageIcon(v.getUrl()));
+			 }
+							
+			
+			return label;
+		}
+	};
+	
 	
 	
 	/*
